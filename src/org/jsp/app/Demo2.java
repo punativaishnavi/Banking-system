4a96701231bc38f9f65c6143870fa23c9f8079ec
punativaishnavi <114441433+punativaishnavi@users.noreply.github.com>
2024-03-22 10:53:27 -0700

    first commit

---
+++ src/org/jsp/app/Demo2.java
@@ -0,0 +1,26 @@
+package org.jsp.app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Demo2 {
+	public static void main(String[] args) 
+	{
+		String  url="jdbc:mysql://localhost:3306?user=root&password=punati";
+		String query="update vaishnavi_db.Registration set password='vaishp' where username='vaishnavi'";
+		try {
+			Connection connection =DriverManager.getConnection(url);
+			System.out.println("step-1");
+			Statement statement=connection.createStatement();
+			System.out.println("step-2");
+			statement.executeUpdate(query);
+			System.out.println("step-3");
+		} catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
